031a080f4278e2f7b766a62ed7f201e721189f81
drfailer <drfailer.42@gmail.com>
2023-12-27 16:17:04 +0100

    add a function to the parser to get the classname

--- serializer/parser.cpp
+++ serializer/parser.cpp
@@ -1,7 +1,12 @@
 #include <stack>
 #include "parser.hpp"
 
-std::size_t findEndValueIndex(std::string str, std::size_t valueStart) {
+/*
+ * findEndValueIndex
+ *
+ * find the index of the end of the value starting at valueStart.
+ */
+std::size_t findEndValueIndex(const std::string& str, std::size_t valueStart) {
     std::size_t idx = valueStart;
     std::stack<char> pairs;
 
@@ -26,6 +31,25 @@ std::size_t findEndValueIndex(std::string str, std::size_t valueStart) {
     return idx;
 }
 
+/*
+ * nextId
+ *
+ * find the index of the nex identifier in str.
+ */
 std::size_t nextId(const std::string& str) {
     return str.find(",") + 2; // ..., id
+}
+
+
+/*
+ * getClassName
+ *
+ * return the name of the class that is deserilized (stored in str).
+ */
+std::string getClassName(const std::string& str) {
+    constexpr std::size_t classNameIdStartIndex = 2; // { __CLASS_NAME__:
+    constexpr std::size_t classNameIdLength = 14; // __CLASS_NAME__
+    constexpr std::size_t valueBegin = classNameIdStartIndex + classNameIdLength + 2; // : value
+    std::size_t valueEnd = findEndValueIndex(str, valueBegin);
+    return str.substr(valueBegin, valueEnd - valueBegin);
 }
--- serializer/parser.hpp
+++ serializer/parser.hpp
@@ -2,7 +2,8 @@
 #define PARSER_HPP
 #include <string>
 
-std::size_t findEndValueIndex(std::string str, std::size_t valueStart);
+std::size_t findEndValueIndex(const std::string& str, std::size_t valueStart);
 std::size_t nextId(const std::string& str);
+std::string getClassName(const std::string& str);
 
 #endif
