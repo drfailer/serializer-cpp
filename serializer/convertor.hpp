db5a5a297094196bb268d02cb8cba25514ac994e
drfailer <drfailer.42@gmail.com>
2024-01-02 15:11:15 +0100

    new helper macro to create deserialize function for polymorphic types more easily

--- serializer/convertor.hpp
+++ serializer/convertor.hpp
@@ -207,6 +207,52 @@ struct Convertor {
               std::enable_if_t<std::is_same_v<base_t<T>, Type>> * = nullptr>   \
     static Type deserialize(input)
 
+#define serialize_custom_type(input)                                           \
+    static std::string serialize(input)
+
 #define class_name(Type) typeid(Type).name()
+
+/******************************************************************************/
+/*                          deserialize polymorphic                           */
+/******************************************************************************/
+
+/*
+ * This macro will generate a deserialize function for polymorphic types. The
+ * purpose is to help the user to easily create its own convertor that is
+ * capable of handling polymorphic types.
+ *
+ * Here we juste have to use the macro DESERIALIZE_POLYMORPHIC. It takes as
+ * arguemnts the super class and the list of classes that inherits from this
+ * super class. The resulting function will return a heap allocated pointer of
+ * type Super class that is created using the right constructor.
+ */
+
+template <typename RT>
+RT type_switch_fn(const std::string &, const std::string &) {
+    return nullptr;
+}
+
+template <typename RT, typename T, typename... Types>
+RT type_switch_fn(const std::string &className, const std::string &str) {
+    static_assert(!std::is_abstract_v<T>, "The type shouldn't be abstract.");
+    if (className == class_name(T)) {
+        T *elt = new T();
+        elt->deserialize(str);
+        return elt;
+    }
+    return type_switch_fn<RT, Types...>(className, str);
+}
+
+/*
+ * Generates a deserialize function for the polymorphic type GenericType.
+ */
+#define DESERIALIZE_POLYMORPHIC(GenericType, ...)                              \
+    template <typename T,                                                      \
+              std::enable_if_t<std::is_same_v<base_t<T>, GenericType *>> * =   \
+                  nullptr>                                                     \
+    static GenericType *deserialize(const std::string &str) {                  \
+        return type_switch_fn<GenericType *, __VA_ARGS__>(                     \
+            getThisClassName(str), str);                                       \
+    }
 
 #endif
