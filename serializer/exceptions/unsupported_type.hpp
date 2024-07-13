9157fe172e14a8543ced42ac7bf58325d2b567dd
drfailer <drfailer.42@gmail.com>
2024-07-13 17:05:31 -0400

    fix: exceptions missing in the previous commit

---
+++ serializer/exceptions/unsupported_type.hpp
@@ -0,0 +1,34 @@
+#ifndef EXCEPTIONS_HPP
+#define EXCEPTIONS_HPP
+#include <exception>
+#include <sstream>
+#include <string>
+
+namespace serializer::exceptions {
+
+template <typename T>
+class UnsupportedTypeError : public std::exception {
+  public:
+    UnsupportedTypeError() {
+//        std::remove_reference<T> *obj;
+        T *obj;
+        std::ostringstream oss;
+        oss << "error: the default convertor doesn't support the type '"
+            << typeid(obj).name() << "'." << std::endl;
+        oss << "Consider making this type serializable or create a custom "
+               "convertor implementation to add support for this type "
+               "manually.";
+        msg = oss.str();
+    }
+
+    const char* what() const noexcept override {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+} // end namespace serializer::exception
+
+#endif
